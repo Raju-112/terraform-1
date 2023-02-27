f812d51e8ba325f3f15a7480b4e9d91f4d0f44ff
Raju-112 <115540555+Raju-112@users.noreply.github.com>
2023-02-27 15:39:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 #AWS as our provider
 
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ap-south-1"
   access_key = "AKIARJNZ7IJBM2ERPDON"
   secret_key = "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"
 }
