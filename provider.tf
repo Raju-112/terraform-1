679c0e2a7f3724b4bdbab09444f55be5e7a724af
Raju-112 <115540555+Raju-112@users.noreply.github.com>
2023-02-27 14:53:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,6 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIARJNZ7IJBMBNBKSAZ"
-  secret_key = "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"
+  access_key = "AKIARJNZ7IJBM2ERPDON"
+  secret_key = "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"
 }
